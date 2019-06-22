6d557ac8bae77c93c85a87de9a334ef5720f0c6d
Kyle Morton <mkmorton91@gmail.com>
2019-06-21 23:14:33 -0500

    created project, working on initial view w/ table and button for movies list

---
+++ favorite-movies-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  favorite-movies-app
+//
+//  Created by Kyle Morton on 6/21/19.
+//  Copyright © 2019 Morton-Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
